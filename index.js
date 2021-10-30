87aaa5f10a087f98e581841166351af9b60aeb6b
DaryaMiller19 <93184244+DaryaMiller19@users.noreply.github.com>
2021-10-30 10:44:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = '2039688992:AAHYLBD8Z5U2ZTVqNLXxN8JW8hByKh0Yecc';
 
-const bot = new TelegramBot
+const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message' , message => {
     const number = Math.round(Math.random() * 20);
